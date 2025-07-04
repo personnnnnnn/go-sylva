87aa46290c1f66ed945c5b031b2797351af397cf
Alexandru Busuioc (Alex) <busuioc.alexandru20@gmail.com>
2025-07-04 23:15:46 +0300

    Fixed mistake in struct.go

--- sylva/struct.go
+++ sylva/struct.go
@@ -7,21 +7,21 @@ type Struct struct {
 	Values []Value
 }
 
-func (t *Struct) GetKey(key string) (Value, error) {
-	for i, k := range t.Keys {
+func (s *Struct) GetKey(key string) (Value, error) {
+	for i, k := range s.Keys {
 		if k == key {
-			return t.Values[i], nil
+			return s.Values[i], nil
 		}
 	}
-	return nil, fmt.Errorf("tuple has no key '%v'", key)
+	return nil, fmt.Errorf("struct has no key '%v'", key)
 }
 
-func (t *Struct) SetKey(key string, value Value) error {
-	for i, k := range t.Keys {
+func (s *Struct) SetKey(key string, value Value) error {
+	for i, k := range s.Keys {
 		if k == key {
-			t.Values[i] = value
+			s.Values[i] = value
 			return nil
 		}
 	}
-	return fmt.Errorf("tuple has no key '%v'", key)
+	return fmt.Errorf("struct has no key '%v'", key)
 }
